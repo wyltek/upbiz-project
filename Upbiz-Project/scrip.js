9211bae284d4cc0313827599566dbd87971e93cd
Wylker Silva <32502005+wyltek@users.noreply.github.com>
2024-02-01 19:00:18 -0300

    Add files via upload

---
+++ Upbiz-Project/scrip.js
@@ -0,0 +1,37 @@
+AOS.init();
+
+function scrollToAbout() {
+    var aboutSection = document.getElementById("about");
+    aboutSection.scrollIntoView({ behavior: 'smooth' });
+    
+}
+
+function scrollToServicos() {
+    var servicosSection = document.getElementById("services");
+    servicosSection.scrollIntoView({ behavior: 'smooth' });
+    
+}
+
+function scrollToReview() {
+    var reviewSection = document.getElementById("review");
+    reviewSection.scrollIntoView({ behavior: 'smooth' });
+    
+}
+
+function scrollToFaq() {
+    var faqSection = document.getElementById("faq");
+    faqSection.scrollIntoView({ behavior: 'smooth' });
+    
+}
+
+function scrollToContact() {
+    var contactSection = document.getElementById("contact");
+    contactSection.scrollIntoView({ behavior: 'smooth' });
+    
+}
+
+function scrollToFaq() {
+    var faqSection = document.getElementById("faq");
+    faqSection.scrollIntoView({ behavior: 'smooth' });
+    
+}
